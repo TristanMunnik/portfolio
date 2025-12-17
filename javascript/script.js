f7aa38192ddc6d025535a739486e1226c359d465
TMunnik <tristanmunnik3@gmail.com>
2025-12-17 13:52:14 +0200

    added return button using javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,24 @@
-console.log("Script loaded successfully.");
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        console.log(`Navigated to ${link.textContent}`);
+    })
+})
+
+const backToTopButton = document.getElementById('back-to-top');
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+const toggleBackToTopButton = () => {
+    if(window.innerWidth > 1100) {
+        backToTopButton.style.display = 'none';
+    } else {
+        backToTopButton.style.display = 'block';
+    }
+}
+
+window.addEventListener('resize', toggleBackToTopButton);
+window.addEventListener('load', toggleBackToTopButton);
